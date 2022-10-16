715c731944c862b6fc29680e1b8f275853d260a8
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-10-16 18:27:14 +0300

    Create 92.js

---
+++ js/basic_javaScript/92.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "BJ",
+        "title": "PM",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ]
+    }
+];
