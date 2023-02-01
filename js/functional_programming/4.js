88e050119f0e98a3fcbc5c28e5ed6e8b13960cb4
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-02-01 16:11:02 +0300

    create functional_programming/4.js

---
+++ js/functional_programming/4.js
@@ -0,0 +1,9 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+    // Only change code below this line
+    const copyFixedValue = fixedValue
+    return copyFixedValue + 1
+    // Only change code above this line
+}
