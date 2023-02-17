836b1e9824e0afab2fe84fdf8bdceb92d9c1489d
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-02-17 15:47:46 +0300

    create functional_programming/19.js

---
+++ js/functional_programming/19.js
@@ -0,0 +1,7 @@
+function splitify(str) {
+    // Only change code below this line
+    return str.split(/[^0-9a-zA-Z]/)
+    // Only change code above this line
+}
+
+splitify('Hello World,I-am code');
